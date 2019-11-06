f4f7848e1b005fc50a0628215f614bc482108507
Nilkamal Shah <nilshah@publicisgroupe.net>
2019-11-06 15:18:56 +0530

    Adding sign in and sign up with firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,20 +2,40 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+const config ={
+  apiKey: "AIzaSyCeCU4waNMUo0caGy5HYmcxVLqJsh_8xzs",
+  authDomain: "crown-db-c763b.firebaseapp.com",
+  databaseURL: "https://crown-db-c763b.firebaseio.com",
+  projectId: "crown-db-c763b",
+  storageBucket: "crown-db-c763b.appspot.com",
+  messagingSenderId: "656706097150",
+  appId: "1:656706097150:web:2ec84ea68e5af3dde8faaf",
+  measurementId: "G-FB3BY7JVWW"
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email, phoneNumber } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ displayName, email, phoneNumber, createdAt,...additionalData });
+    } catch (error) {
+      console.log('Error occurred while creating user', error.message);
+    }
+
+  }
+  return userRef;
+}
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
